feb52e44ae44ed25adf28b557b65580e2a9df4d7
Patryk Martynowicz <patryk.martynowicz@displate.com>
2022-06-18 22:54:46 +0200

    feat: initialize Symfony

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo 'PHPers Summit 2022';
+use Displate\Phpers\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
